9576bfd54338a265aeb72b9df67dd80c419c3aa2
Wilder Rincon <wildsrincon.developer@gmail.com>
2024-01-25 09:38:21 -0500

    refactor: add typescript entire project

--- src/ClientApp.jsx
+++ src/ClientApp.jsx
@@ -1,4 +1,4 @@
-import { hydrateRoot } from "react-dom";
+import { hydrateRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
--- src/Details.jsx
+++ src/Details.jsx
@@ -1,6 +1,7 @@
 import { useState, useContext, lazy } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query';
+import { PetAPIResponse } from './APIResponseTypes';
 import ErrorBoundary from './ErrorBoundary';
 import AdoptedPetContext from './AdoptedPetContext';
 import Carousel from './Carousel';
@@ -10,25 +11,30 @@ const Modal = lazy(() => import('./Modal'));
 
 const Details = () => {
   const [showModal, setShowModal] = useState(false);
-  const [, setAdoptedPet] = useContext(AdoptedPetContext);
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unused-vars
+  const [_, setAdoptedPet] = useContext(AdoptedPetContext);
   const { id } = useParams();
   const navigate = useNavigate();
-  const results = useQuery(['details', id], fetchPet);
+  const results = useQuery<PetAPIResponse>(['details', id], fetchPet);
 
   if (results.isLoading) {
     return (
-      <div className="loading-pane">
-        <h2 className="loader">🌀</h2>
+      <div className="mx-auto h-screen flex justify-center items-center p-4">
+        <h2 className="animate-spin text-[100px]">🌀</h2>
       </div>
     );
   }
 
-  const pet = results.data.pets[0];
+  const pet = results?.data?.pets[0];
   const handleBack = () => navigate('/', { replace: true });
+
+  if (!pet) {
+    throw new Error('pet not found')
+  }
 
   return (
     <div className="container mx-auto w-11/12 bg-red-100 rounded-md pt-1">
-      <Carousel images={pet.images} />
+      <Carousel images={pet?.images} />
       <div className='flex flex-col justify-center items-center'>
         <h1 className='text-center text-black font-bold text-[60px] mt-2'>{pet.name}</h1>
         <h2>{`${pet.animal} — ${pet.breed} — ${pet.city}, ${pet.state}`}</h2>
